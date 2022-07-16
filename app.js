a4002383b218b894befde729a4256a66b0c1f5d7
Cyber-Borries <69439807+Cyber-Borries@users.noreply.github.com>
2022-07-16 10:07:50 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+"use strict";
+
+const myArray = ["rock", "paper", "scissors"];
+let playerSelection = prompt(
+  "Please write rock, paper or scissors"
+).toLocaleLowerCase();
+let computerSelection = myArray[(Math.random() * myArray.length) | 0];
+let playerWinCount = 0;
+let computerWinCount = 0;
+let roundResult = 0;
+
+console.log("You chose " + playerSelection);
+console.log("The computer chose " + computerSelection);
+console.log("New round");
+playRound(playerSelection, computerSelection);
+game();
+
+function computerPlay() {
+  let computerTurn = myArray[(Math.random() * myArray.length) | 0];
+  console.log(computerTurn);
+  // return computerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+  //playerSelection rock
+
+  if (playerSelection === "rock" && computerSelection === "rock") {
+    alert("It's a tie");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    alert("You lose! Paper beats rock");
+    computerWinCount++;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    alert("You win! Rock beats scissors");
+    playerWinCount++;
+  }
+  //playerSelection paper
+  else if (playerSelection === "paper" && computerSelection === "paper") {
+    alert("It's a tie");
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    alert("You lose! scissors beats paper");
+    computerWinCount++;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    alert("You win! Paper beats rock");
+    playerWinCount++;
+  }
+  //playerSelection scissors
+  else if (playerSelection === "scissors" && computerSelection === "scissors") {
+    alert("It's a tie");
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    alert("You lose! Rock beats scissors");
+    computerWinCount++;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    alert("You win! Scissors beats paper");
+    playerWinCount++;
+  }
+  return roundResult;
+}
+console.log("You have won " + playerWinCount + " rounds");
+console.log("The computer has won " + computerWinCount + " rounds");
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playerSelection = prompt(
+      "Please write rock, paper or scissors"
+    ).toLocaleLowerCase();
+    computerSelection = myArray[(Math.random() * myArray.length) | 0];
+    playRound(playerSelection, computerSelection);
+    console.log("You chose " + playerSelection);
+    console.log("The computer chose " + computerSelection);
+    console.log("New round");
+  }
+  playRound();
+  if (playerWinCount > computerWinCount) {
+    alert(
+      `You won ${playerWinCount} rounds and the computer won ${computerWinCount} You won`
+    );
+  } else {
+    alert(
+      `The computer won ${computerWinCount} rounds and you won ${playerWinCount} rounds. You lost by `
+    );
+  }
+}
+
+// computerSelection = computerPlay();
+// playerSelection = prompt(
+//   "Please write rock, paper or scissors"
+// ).toLocaleLowerCase();
+// let roundResult;
